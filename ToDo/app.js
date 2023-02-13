fd4dd5a9b98285c6d267edd2d0ef30801549d2f6
Gus <g.himenos@gmail.com>
2023-02-13 04:29:03 +0100

    serializer and renderer done

--- ToDo/app.js
+++ ToDo/app.js
@@ -1,14 +1,42 @@
-const task = document.querySelector('#task-content');
+//Todo Хард-код - если таких штук много - можно обойти циклом
 
-task.addEventListener('submit', formSerializer);
+const task = document.querySelector("#task-content");
+task.addEventListener("submit", formSerializer);
 
-function formSerializer(event){
+//Todo ===Конец===Хард-код===================================
 
-    event.preventDefault();
-    let values = this.elements;
-    values=Array.from(values);
-    
-    values.forEach((val)=>
-        console.log(key)
-    )
-}   
+let todosArr =[];
+let newAdded = false;
+
+renderTodos();
+
+function formSerializer(event) {
+  event.preventDefault();
+  let input = new FormData(this);
+  let output = Object.fromEntries(input);
+  todosArr.push(output);
+  newAdded = true;
+  console.log(newAdded);//*проверка
+  renderTodos();
+  task.reset();
+}
+
+function renderTodos(){
+
+    if(newAdded){
+
+        console.log('works with true value');//*проверка
+        console.log(todosArr);//*проверка
+        localStorage.clear();
+        localStorage.setItem('todos', JSON.stringify(todosArr));
+        todosArr= JSON.parse(localStorage.getItem('todos'));
+        console.log(todosArr);//*проверка
+    }
+    else{
+        console.log('works with false value');//*проверка
+        let newArr= JSON.parse(localStorage.getItem('todos'));
+        !newArr ? todosArr=[] : todosArr=newArr;
+        console.log(todosArr);//*проверка
+    }
+
+}
